4b7b05a12473bad90884eeaf2468c24c4ded6cd4
Steven <jgu8@u.rochester.edu>
2020-01-07 15:04:34 -0500

    add home page

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    date = models.DateField(auto_now_add=True)
-    body = models.CharField(max_length=10000)
+    date = models.DateTimeField()
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
     
